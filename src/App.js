6be4cd514d911e39618518eb1736f40408025b80
Vitalii <vitaliy.kalmykov.work@gmail.com>
2025-04-03 13:57:52 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,8 +14,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React fast fast
         </a>
+        <p>asd</p>
+        <p>asd  </p>
       </header>
     </div>
   );
